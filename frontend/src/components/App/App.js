b1bf9bbb77e63c1dbf1747be4a7b70c064cd341c
samicap <lovestochasewindmills@gmail.com>
2021-03-11 02:54:57 -0500

    add condition for Login and token

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <div className="wrapper">
       <h1>Orange</h1>
