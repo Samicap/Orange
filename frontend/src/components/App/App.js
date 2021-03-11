f8f50041bba3b3efa6c53f292bb3ec25280a1632
samicap <lovestochasewindmills@gmail.com>
2021-03-11 01:01:51 -0500

    moved folder and added new components

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="wrapper">
       <h1>Orange</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
